5857b9267c73f23d2a3fd78cff3c2eb12472dee8
PedroEmanuel22 <pedro106emanuel@gmail.com>
2026-06-27 16:17:41 -0300

    class aluno
    criei e testei a classe aluno

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from livro import Livro
+from aluno import Aluno
 
 livro = Livro("Dom Casmurro", "Machado de Assis")
+aluno = Aluno("Pedro", "20251174010002")
 
 print(livro)
-print(livro.disponivel)
-
-livro.emprestar()
-
-print(livro.disponivel)
+print(aluno)
