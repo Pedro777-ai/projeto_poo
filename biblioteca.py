c98f95f1cb8bde88bb89ff08813e13cf8fbb1bf0
PedroEmanuel22 <pedro106emanuel@gmail.com>
2026-06-27 16:32:07 -0300

    class biblioteca
    criei e testei a classe biblioteca que gera duas listas vazias

--- biblioteca.py
+++ biblioteca.py
@@ -1,1 +1,12 @@
 # Controla os livros e alunos
+
+class Biblioteca:
+    def __init__(self):
+        self.livros = []
+        self.alunos = []
+
+    def adicionar_livro(self, livro):
+        self.livros.append(livro)
+
+    def adicionar_aluno(self, aluno):
+        self.alunos.append(aluno)
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from livro import Livro
 from aluno import Aluno
+from biblioteca import Biblioteca
 
-livro = Livro("Dom Casmurro", "Machado de Assis")
-aluno = Aluno("Pedro", "20251174010002")
+biblioteca = Biblioteca()
 
-print(livro)
-print(aluno)
+livro1 = Livro("Dom Casmurro", "Machado de Assis")
+livro2 = Livro( "1984", "George Orwell")
+
+aluno1 = Aluno("Pedro", "20251174010002")
+aluno2 = Aluno("Igor", "2025117401035")
+
+biblioteca.adicionar_livro(livro1)
+biblioteca.adicionar_livro(livro2)
+biblioteca.adicionar_aluno(aluno1)
+biblioteca.adicionar_aluno(aluno2)
+
+print("Livros cadastrados")
+for livro in biblioteca.livros:
+    print(livro)
+
+print("\nAlunos cadastrados")
+for aluno in biblioteca.alunos:
+    print(aluno)
+
+
+
