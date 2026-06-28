9d0e9710032c5e1520474d36132dc211ca7392a0
PedroEmanuel22 <pedro106emanuel@gmail.com>
2026-06-28 15:10:47 -0300

    interface
    criei e testei a primeira janela

--- interface.py
+++ interface.py
@@ -1,4 +1,12 @@
 # Parte visual 
 
 import tkinter as tk
-from tkinter import tkk
+from tkinter import ttk
+
+class Interface:
+    def __init__(self):
+        self.janela = tk.Tk()
+        self.janela.title("Biblioteca Escolar")
+        self.janela.geometry("800x600")
+
+        self.janela.mainloop()
--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 from livro import Livro
 from aluno import Aluno
 from biblioteca import Biblioteca
+from interface import Interface
 
-biblioteca = Biblioteca()
-
-livro1 = Livro("Dom Casmurro", "Machado de Assis")
-livro2 = Livro( "1984", "George Orwell")
-
-aluno1 = Aluno("Pedro", "20251174010002")
-aluno2 = Aluno("Igor", "20251174010035")
-
-biblioteca.adicionar_livro(livro1)
-biblioteca.adicionar_livro(livro2)
-biblioteca.adicionar_aluno(aluno1)
-biblioteca.adicionar_aluno(aluno2)
-
-print("Livros cadastrados:")
-for livro in biblioteca.livros:
-    print(livro)
-
-print("\nAlunos cadastrados:")
-for aluno in biblioteca.alunos:
-    print(aluno)
-
+Interface()
 
 
