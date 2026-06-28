89dc399ff5f465a672acc1332cb617e82c6728f5
PedroEmanuel22 <pedro106emanuel@gmail.com>
2026-06-28 15:19:44 -0300

    interface
    adicionei 3 abas a interface

--- interface.py
+++ interface.py
@@ -7,6 +7,17 @@ class Interface:
     def __init__(self):
         self.janela = tk.Tk()
         self.janela.title("Biblioteca Escolar")
-        self.janela.geometry("800x600")
+        self.janela.geometry("900x600")
+
+        self.notebook = ttk.Notebook(self.janela)
+        self.notebook.pack(fill="both", expand=True)
+
+        self.aba_livros = ttk.Frame(self.notebook)
+        self.aba_alunos = ttk.Frame(self.notebook)
+        self.aba_emprestimos = ttk.Frame(self.notebook)
+
+        self.notebook.add(self.aba_livros, text="Livros")
+        self.notebook.add(self.aba_alunos, text="Alunos")
+        self.notebook.add(self.aba_emprestimos, text="Empréstimos")
 
         self.janela.mainloop()
